21d50e4a8251bd1eb98a5578a56e91484ef5885e
Hannes Mehnert <hannes@mehnert.org>
2019-07-05 19:00:50 +0200

    .

--- .travis-test-mirage.sh
+++ .travis-test-mirage.sh
@@ -5,9 +5,8 @@ eval `opam config env`
 opam repo add -y future git+https://github.com/roburio/git-ssh-dns-mirage3-repo.git
 opam update
 opam upgrade -y
-
-opam install mirage
+opam install -y mirage
 
 cd mirage
-mirage configure && make depend && mirage build && mirage clean
+mirage configure && make depend && mirage build && mirage clean &&
 mirage configure -t hvt && make depend && mirage build && mirage clean
