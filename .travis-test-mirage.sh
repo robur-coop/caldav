b66e7c9be372e9586e169eab8904191719647ff2
Hannes Mehnert <hannes@mehnert.org>
2019-07-05 18:42:02 +0200

    test mirageos unikernel in travis

---
+++ .travis-test-mirage.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+eval `opam config env`
+
+opam repo add -y future git+https://github.com/roburio/git-ssh-dns-mirage3-repo.git
+opam update
+opam upgrade -y
+
+opam install mirage
+
+cd mirage
+mirage configure && make depend && mirage build && mirage clean
+mirage configure -t hvt && make depend && mirage build && mirage clean
