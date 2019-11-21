b63570d2252bc29613be20925946005394da28f5
Hannes Mehnert <hannes@mehnert.org>
2019-11-21 17:01:34 +0100

    since merged to master, no need for future2 branch pin

--- mirage/config.ml
+++ mirage/config.ml
@@ -40,7 +40,7 @@ let apple_testable =
 let main =
   let direct_dependencies = [
     package "uri" ;
-    package ~pin:"git+https://github.com/roburio/caldav.git#future2" "caldav" ;
+    package ~pin:"git+https://github.com/roburio/caldav.git" "caldav" ;
     package ~min:"0.1.3" "icalendar" ;
     package ~min:"2.0.0" "irmin-git" ;
     package ~min:"2.0.0" "irmin-mirage-git" ;
