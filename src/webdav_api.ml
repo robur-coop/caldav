robur-coop/caldav

commit 1e4bf3d1085a675e54ee5b38e8d916c3a55d65ce

drop rresult and astring dependency

src/webdav_api.ml
@@ -786,12 +786,6 @@ module Make(R : Mirage_random.S)(Clock : Mirage_clock.PCLOCK)(Fs: Webdav_fs.S) =
       (* TODO: treat pfs *)
       matches_timerange && matches_cfs
 
-  let get_timezones_for_resp calendar tzids =
-    let get_timezone tzid =
-      let has_matching_tzid props = List.exists (function `Timezone_id (_, (_, tzid')) -> tzid = tzid' | _ -> false) props in
-      List.find (function `Timezone props -> has_matching_tzid props | _ -> false ) (snd calendar) in
-    List.map get_timezone @@ Astring.String.Set.elements tzids
-
   (* returns the entire calendar if any component matches the component filter.
      this is fine, because in caldav rfc and apple testsuite, each calendar only uses one component per file *)
   let vcalendar_matches_comp_filter filter (props, comps) =
@@ -975,14 +969,17 @@ let hash_password password salt =
   base64_encode @@ Mirage_crypto.Hash.SHA256.digest @@ Cstruct.of_string (salt ^ "-" ^ password)
 
 let verify_auth_header fs config v =
-  match Astring.String.cut ~sep:"Basic " v with
-  | Some ("", b64) ->
-    begin match Base64.decode b64 with
-      | Error `Msg msg ->
-        Lwt.return @@ Rresult.R.error_msgf "invalid base64 encoding %s: %s" msg b64
-      | Ok data -> match Astring.String.cut ~sep:":" data with
-        | None -> Lwt.return @@ Error (`Msg ("invalid user:pass encoding" ^ data))
-        | Some (user, password) ->
+  let basic = "Basic " in
+  let blen = String.length basic in
+  let vlen = String.length v in
+  if vlen >= blen && String.(equal (sub v 0 blen) basic) then
+    let b64 = String.sub v blen (vlen - blen) in
+    match Base64.decode b64 with
+    | Error `Msg msg ->
+      Lwt.return (Error (`Msg (Fmt.str "invalid base64 encoding %s: %s" msg b64)))
+    | Ok data -> match String.split_on_char ':' data with
+      | [user; password] ->
+        begin
           Fs.get_property_map fs (`Dir [config.principals ; user]) >|= fun props ->
           (* no user context yet *)
           match
@@ -995,8 +992,10 @@ let verify_auth_header fs config v =
             then Ok user
             else Error (`Msg "password does not match")
           | _ -> Error (`Unknown_user (user, password))
-    end
-  | _ -> Lwt.return @@ Error (`Msg ("invalid auth header " ^ v))
+        end
+      | _ -> Lwt.return @@ Error (`Msg ("invalid user:pass encoding" ^ data))
+  else
+    Lwt.return @@ Error (`Msg ("invalid auth header " ^ v))
 
 let last_modified fs ~path =
   Fs.from_string fs path >>= function
@@ -1132,10 +1131,12 @@ let href_to_principal principals_directory tree =
   | Ok principal ->
     let path = Uri.path (Uri.of_string principal) in
     let dir = Fs.to_string (`Dir [principals_directory]) in
-    if Astring.String.is_prefix ~affix:dir path then
-      match Astring.String.cut ~sep:dir path with
-      | Some ("", p) -> Ok p
-      | _ -> Error "invalid path"
+    let dlen = String.length dir
+    and plen = String.length path
+    in
+    if plen >= dlen && String.(equal (sub path 0 dlen) dir) then
+      let p = String.sub path 0 dlen in
+      Ok p
     else
       Error "invalid path"
   | Error msg -> Error msg

app/dune
@@ -1,3 +1,3 @@
 (test
  (name caldav_server)
- (libraries logs logs.fmt caldav webmachine lwt ptime ptime.clock.os cohttp cohttp-lwt-unix mirage-kv rresult oUnit mirage-clock-unix tcpip tcpip.stack-socket mirage-random-test mirage-kv-mem))
+ (libraries logs logs.fmt caldav webmachine lwt ptime ptime.clock.os cohttp cohttp-lwt-unix mirage-kv oUnit mirage-clock-unix tcpip tcpip.stack-socket mirage-random-test mirage-kv-mem))

caldav.opam
@@ -21,7 +21,7 @@ build: [
 ]
 
 depends: [
-  "ocaml" {>= "4.07.0"}
+  "ocaml" {>= "4.08.0"}
   "dune" {>= "1.3"}
   "alcotest" {with-test & >= "0.8.5"}
   "ounit" {with-test & >= "2.0.0"}
@@ -29,6 +29,7 @@ depends: [
   "tcpip" {with-test & >= "3.7.0"}
   "mirage-clock-unix" {with-test & >= "2.0.0"}
   "mirage-kv-mem" {with-test & >= "2.0.0"}
+  "fmt" {>= "0.8.7"}
   "mirage-kv" {>= "3.0.0"}
   "mirage-clock" {>= "2.0.0"}
   "mirage-random" {>= "2.0.0"}
@@ -44,15 +45,14 @@ depends: [
   "xmlm" {>= "1.3.0"}
   "tyxml" {>= "4.3.0"}
   "icalendar" {>= "0.1.2"}
-  "rresult" {>= "0.6.0"}
   "sexplib" {>= "v0.12.0"}
   "ppx_sexp_conv" {>= "v0.12.0"}
   "logs" {>= "0.6.3"}
   "hex" {>= "1.4.0"}
   "metrics"
+  "re" {>= "1.7.2"}
   #from webmachine
   "dispatch" {>= "0.5.0"}
-  "re" {>= "1.7.2"}
   "uri" {>= "4.0.0"}
 ]
 synopsis: "A CalDAV server"

src/dune
@@ -2,4 +2,4 @@
   (name        caldav)
   (public_name caldav)
   (preprocess (pps ppx_deriving.std ppx_sexp_conv))
-  (libraries logs lwt ptime mirage-kv cohttp xmlm tyxml icalendar rresult mirage-crypto mirage-crypto-rng base64 caldav.webmachine cohttp-lwt mirage-clock mirage-random sexplib hex metrics))
+  (libraries logs lwt ptime mirage-kv cohttp xmlm tyxml icalendar mirage-crypto mirage-crypto-rng base64 caldav.webmachine cohttp-lwt mirage-clock mirage-random sexplib hex metrics))

src/webdav_fs.ml
@@ -99,7 +99,9 @@ module Make (Pclock : Mirage_clock.PCLOCK) (Fs:Mirage_kv.RW) = struct
     `File (data @ [name])
 
   (* TODO: no handling of .. done here yet *)
-  let data_to_list str = Astring.String.cuts ~empty:false ~sep:"/" str
+  let data_to_list str =
+    String.split_on_char '/' str |>
+    List.filter_map (function "" -> None | x -> Some x)
   let data str = Mirage_kv.Key.v str
 
   let dir_from_string str = `Dir (data_to_list str)
@@ -107,7 +109,7 @@ module Make (Pclock : Mirage_clock.PCLOCK) (Fs:Mirage_kv.RW) = struct
   let file_from_string str = `File (data_to_list str)
 
   let to_string =
-    let a = Astring.String.concat ~sep:"/" in
+    let a = String.concat "/" in
     function
     | `File data -> "/" ^ a data
     | `Dir data -> "/" ^ a data ^ "/"
@@ -142,7 +144,7 @@ module Make (Pclock : Mirage_clock.PCLOCK) (Fs:Mirage_kv.RW) = struct
     | `File data -> match List.rev data with
       | filename :: path -> List.rev path @ [ filename ^ propfile_ext ]
       | [] -> assert false (* no file without a name *) in
-    Mirage_kv.Key.v (Astring.String.concat ~sep:"/" segments)
+    Mirage_kv.Key.v (String.concat "/" segments)
 
   let get_properties fs f_or_d =
     let propfile = propfilename f_or_d in
@@ -187,7 +189,10 @@ module Make (Pclock : Mirage_clock.PCLOCK) (Fs:Mirage_kv.RW) = struct
     | Error e -> Error e
     | Ok files ->
       let files = List.fold_left (fun acc (step, kind) ->
-          if Astring.String.is_suffix ~affix:propfile_ext step then
+          let slen = String.length step
+          and plen = String.length propfile_ext
+          in
+          if slen >= plen && String.(equal (sub step (slen - plen) plen) propfile_ext) then
             acc
           else
             (* TODO check whether step is the entire path, or dir needs to be included *)

src/webdav_server.ml
@@ -49,8 +49,12 @@ let http_status =
   let src = counter_metrics ~f "http_response" in
   (fun r -> Metrics.add src (fun x -> x) (fun d -> d r))
 
+let is_alphanum = function
+  | 'a'..'z' | '0'..'9' | 'A'..'Z' -> true
+  | _ -> false
+
 let sane username =
-  username <> "" && Astring.String.for_all Astring.Char.Ascii.is_alphanum username
+  username <> "" && Xml.for_all is_alphanum username
 
 module Headers = struct
   let get_content_type headers =
@@ -71,8 +75,8 @@ module Headers = struct
     | None -> false
     | Some x ->
       (* Apple seems to use the regular expression 'Mozilla/.*Gecko.*' *)
-      Astring.String.is_prefix ~affix:"Mozilla/" x &&
-      Astring.String.is_infix ~affix:"Gecko" x
+      let re = Re.compile (Re.Perl.re "^Mozilla/.*Gecko.*") in
+      Re.execp re x
 
   let replace_location location headers =
     Cohttp.Header.replace headers "Location" (Uri.to_string @@ location)
@@ -415,7 +419,7 @@ module Make (R : Mirage_random.S) (Clock : Mirage_clock.PCLOCK) (Fs : Webdav_fs.
       match Uri.get_query_param rd.uri "members" with
       | None -> Ok []
       | Some x ->
-        let members = Astring.String.cuts ~sep:"," x in
+        let members = String.split_on_char ',' x in
         if List.for_all sane members
         then Ok members
         else Error `Bad_request
@@ -591,7 +595,7 @@ module Make (R : Mirage_random.S) (Clock : Mirage_clock.PCLOCK) (Fs : Webdav_fs.
 (*    Access_log.debug (fun m -> m "%s %s path: %s"
                          (Cohttp.Code.string_of_method (Cohttp.Request.meth request))
                          (Uri.path (Cohttp.Request.uri request))
-                         (Astring.String.concat ~sep:", " path)) ; *)
+                         (String.concat ", " path)) ; *)
     Time_log.debug (fun m -> m "%s\t%s\t%d\t%f"
                          (Cohttp.Code.string_of_method (Cohttp.Request.meth request))
                          (Cohttp.Request.resource request)

src/webdav_xml.ml
@@ -1,6 +1,6 @@
 [@@@ocaml.warning "-27"]
 
-open Rresult.R.Infix
+let ( let* ) = Result.bind
 
 module M = Map.Make(String)
 
@@ -245,8 +245,17 @@ let tree_to_tyxml t =
 
 let tree_to_string t = tyxml_to_body @@ tree_to_tyxml t
 
+(* from OCaml 4.13 bytes.ml *)
+let for_all p s =
+  let n = String.length s in
+  let rec loop i =
+    if i = n then true
+    else if p (String.unsafe_get s i) then loop (succ i)
+    else false in
+  loop 0
+
 let is_whitespace_node = function
-  | Pcdata str -> Astring.String.for_all (function ' ' | '\n' | '\r' | '\t' -> true | _ -> false) str
+  | Pcdata str -> for_all (function ' ' | '\n' | '\r' | '\t' -> true | _ -> false) str
   | Node _ -> false
 
 let string_to_tree str =
@@ -277,7 +286,7 @@ let rec filter_map f = function
 
 let tree_lift f node_p children_p =
   (fun tree ->
-     node_p tree >>= fun node ->
+     let* node = node_p tree in
      let ch' = match tree with
        | Node (_, _, _, ch) -> filter_map children_p ch
        | Pcdata _           -> []
@@ -352,56 +361,56 @@ let href_parser =
     extract_pcdata
 
 let privilege_children_parser =
-  (tree_lift (fun _ c -> is_empty c >>| fun _ -> `Read)
+  (tree_lift (fun _ c -> let* _ = is_empty c in Ok `Read)
      (name_ns "read" dav_ns) any)
-  ||| (tree_lift (fun _ c -> is_empty c >>| fun _ -> `Write)
+  ||| (tree_lift (fun _ c -> let* _ = is_empty c in Ok `Write)
          (name_ns "write" dav_ns) any)
-  ||| (tree_lift (fun _ c -> is_empty c >>| fun _ -> `Write_content)
+  ||| (tree_lift (fun _ c -> let* _ = is_empty c in Ok `Write_content)
          (name_ns "write-content" dav_ns) any)
-  ||| (tree_lift (fun _ c -> is_empty c >>| fun _ -> `Write_properties)
+  ||| (tree_lift (fun _ c -> let* _ = is_empty c in Ok `Write_properties)
          (name_ns "write-properties" dav_ns) any)
-  ||| (tree_lift (fun _ c -> is_empty c >>| fun _ -> `Unlock)
+  ||| (tree_lift (fun _ c -> let* _ = is_empty c in Ok `Unlock)
          (name_ns "unlock" dav_ns) any)
-  ||| (tree_lift (fun _ c -> is_empty c >>| fun _ -> `Read_acl)
+  ||| (tree_lift (fun _ c -> let* _ = is_empty c in Ok `Read_acl)
          (name_ns "read-acl" dav_ns) any)
-  ||| (tree_lift (fun _ c -> is_empty c >>| fun _ -> `Read_current_user_privilege_set)
+  ||| (tree_lift (fun _ c -> let* _ = is_empty c in Ok `Read_current_user_privilege_set)
          (name_ns "read-current-user-privilege-set" dav_ns) any)
-  ||| (tree_lift (fun _ c -> is_empty c >>| fun _ -> `Write_acl)
+  ||| (tree_lift (fun _ c -> let* _ = is_empty c in Ok `Write_acl)
          (name_ns "write-acl" dav_ns) any)
-  ||| (tree_lift (fun _ c -> is_empty c >>| fun _ -> `Bind)
+  ||| (tree_lift (fun _ c -> let* _ = is_empty c in Ok `Bind)
          (name_ns "bind" dav_ns) any)
-  ||| (tree_lift (fun _ c -> is_empty c >>| fun _ -> `Unbind)
+  ||| (tree_lift (fun _ c -> let* _ = is_empty c in Ok `Unbind)
          (name_ns "unbind" dav_ns) any)
-  ||| (tree_lift (fun _ c -> is_empty c >>| fun _ -> `All)
+  ||| (tree_lift (fun _ c -> let* _ = is_empty c in Ok `All)
          (name_ns "all" dav_ns) any)
 
 let privilege_parser =
-  tree_lift (fun _ c -> non_empty c >>| fun () -> c)
+  tree_lift (fun _ c -> let* () = non_empty c in Ok c)
     (name_ns "privilege" dav_ns) privilege_children_parser
 
 let xml_to_ace : tree -> (ace, string) result =
   let principal = (* TODO other principals, property, invert *)
     tree_lift
-      (fun _ c -> exactly_one c >>| fun c' -> `Principal c')
+      (fun _ c -> let* c' = exactly_one c in Ok (`Principal c'))
       (name_ns "principal" dav_ns)
-      ((tree_lift (fun _ c -> is_empty c >>| fun () -> `All)
+      ((tree_lift (fun _ c -> let* () = is_empty c in Ok `All)
           (name_ns "all" dav_ns) any)
-       ||| (tree_lift (fun _ c -> is_empty c >>| fun () -> `Authenticated)
+       ||| (tree_lift (fun _ c -> let* () = is_empty c in Ok `Authenticated)
               (name_ns "authenticated" dav_ns) any)
-       ||| (tree_lift (fun _ c -> is_empty c >>| fun () -> `Unauthenticated)
+       ||| (tree_lift (fun _ c -> let* () = is_empty c in Ok `Unauthenticated)
               (name_ns "unauthenticated" dav_ns) any)
-       ||| (tree_lift (fun _ c -> is_empty c >>| fun () -> `Self)
+       ||| (tree_lift (fun _ c -> let* () = is_empty c in Ok `Self)
               (name_ns "self" dav_ns) any)
        ||| (href_parser >>~ fun href -> Ok (`Href (Uri.of_string href))))
   and grant_or_deny =
     tree_lift
-      (fun _ c -> exactly_one c >>| fun c' -> `Grant c')
+      (fun _ c -> let* c' = exactly_one c in Ok (`Grant c'))
       (name_ns "grant" dav_ns) privilege_parser
-    ||| tree_lift (fun _ c -> exactly_one c >>| fun c' -> `Deny c')
+    ||| tree_lift (fun _ c -> let* c' = exactly_one c in Ok (`Deny c'))
       (name_ns "deny" dav_ns) privilege_parser
   and inherited =
     tree_lift
-      (fun _ c -> exactly_one c >>| fun c' -> `Inherited c')
+      (fun _ c -> let* c' = exactly_one c in Ok (`Inherited c'))
       (name_ns "inherited" dav_ns)
       (href_parser >>~ fun href -> Ok (Uri.of_string href))
   in
@@ -462,16 +471,16 @@ let ace_to_xml (principal, privilege) =
 
 let propfind_prop_parser : tree -> ([ propfind | `Include of string list ], string) result =
   ((tree_lift
-      (fun _ c -> is_empty c >>| fun () -> `Propname)
+      (fun _ c -> let* () = is_empty c in Ok `Propname)
       (name_ns "propname" dav_ns) any)
    ||| (tree_lift
-          (fun _ c -> non_empty c >>| fun () -> `Props c)
+          (fun _ c -> let* () = non_empty c in Ok (`Props c))
           (name_ns "prop" dav_ns) (any >>~ extract_ns_name))
    ||| (tree_lift
-          (fun _ c -> is_empty c >>| fun () -> `All_prop [])
+          (fun _ c -> let* () = is_empty c in Ok (`All_prop []))
           (name_ns "allprop" dav_ns) any)
    ||| (tree_lift
-          (fun _ c -> non_empty c >>| fun () -> `Include c)
+          (fun _ c -> let* () = non_empty c in Ok (`Include c))
           (name_ns "include" dav_ns) (any >>~ extract_name)))
 
 let parse_propfind_xml tree =
@@ -528,7 +537,8 @@ let rec comp_parser tree : (component, string) result =
          else
            Error "broken")
     (name_ns "comp" caldav_ns >>~ extract_attributes)
-    ((tree_lift (fun _ c -> is_empty c >>| fun () -> `Allprop) (name_ns "allprop" caldav_ns) any)
+    ((tree_lift (fun _ c -> let* () = is_empty c in Ok `Allprop)
+        (name_ns "allprop" caldav_ns) any)
      ||| (tree_lift
             (fun a c ->
                let novalue = match find_attribute "novalue" a with
@@ -537,16 +547,19 @@ let rec comp_parser tree : (component, string) result =
                in
                match find_attribute "name" a with
                | None -> Error "No name in prop"
-               | Some name' -> is_empty c >>| fun () -> (`Prop (name', novalue)))
+               | Some name' -> let* () = is_empty c in Ok (`Prop (name', novalue)))
             (name_ns "prop" caldav_ns >>~ extract_attributes) any)
-     ||| (tree_lift (fun _ c -> is_empty c >>| fun () -> `Allcomp) (name_ns "allcomp" caldav_ns) any)
+     ||| (tree_lift (fun _ c -> let* () = is_empty c in Ok `Allcomp)
+            (name_ns "allcomp" caldav_ns) any)
      ||| (comp_parser >>~ fun (s, p, c) -> Ok (`Comp (s, p, c))))
     tree
 
 (* RFC 4791 Section 9.9: Both attributes MUST be specified as "date with UTC time" value. *)
 let range_parser name : tree -> (timerange, string) result =
   tree_lift
-    (fun a c -> is_empty c >>= fun () -> match find_attribute "start" a, find_attribute "end" a with
+    (fun a c ->
+       let* () = is_empty c in
+       match find_attribute "start" a, find_attribute "end" a with
        | None, _ | _, None -> Error "Missing attribute \"start\" or \"end\""
        | Some s, Some e -> match Icalendar.parse_datetime s, Icalendar.parse_datetime e with
          | Ok (`Utc s'), Ok (`Utc e') ->
@@ -597,27 +610,27 @@ let extract_and_tag_prop = function
 
 let report_prop_parser : tree -> (report_prop, string) result =
   (tree_lift
-     (fun _ c -> is_empty c >>| fun () -> `Propname)
+     (fun _ c -> let* () = is_empty c in Ok `Propname)
      (name_ns "propname" dav_ns) any)
   ||| (tree_lift
-         (fun _ c -> is_empty c >>| fun () -> `All_props)
+         (fun _ c -> let* () = is_empty c in Ok `All_props)
          (name_ns "allprop" dav_ns) any)
   ||| (tree_lift
-         (fun _ c -> non_empty c >>| fun () -> `Proplist c)
+         (fun _ c -> let* () = non_empty c in Ok (`Proplist c))
          (name_ns "prop" dav_ns)
          ((calendar_data_parser >>~ fun c -> Ok (`Calendar_data c))
           ||| (any >>~ extract_and_tag_prop)))
 
 let is_not_defined_parser =
   tree_lift
-    (fun _ c -> is_empty c >>| fun () -> `Is_not_defined)
+    (fun _ c -> let* () = is_empty c in Ok `Is_not_defined)
     (name_ns "is-not-defined" caldav_ns)
     any
 
 let text_match_parser =
   tree_lift
     (fun a c ->
-      exactly_one c >>| fun str ->
+      let* str = exactly_one c in
       let collation =
         match find_attribute "collation" a with
         | None -> "i;ascii-casemap"
@@ -628,7 +641,7 @@ let text_match_parser =
         | Some "yes" -> true
         | _ -> false
       in
-      `Text_match (str, collation, negate)
+      Ok (`Text_match (str, collation, negate))
     )
     (name_ns "text-match" caldav_ns >>~ extract_attributes)
     extract_pcdata
@@ -660,8 +673,12 @@ let prop_filter_parser : tree -> (prop_filter, string) result =
       | Some n -> match c with
         | [] -> Ok (n, `Is_defined)
         | [ `Is_not_defined ] -> Ok (n, `Is_not_defined)
-        | `Timerange t :: pfs -> all_param_filters pfs >>| fun pfs' -> (n, `Range (t, pfs'))
-        | `Text_match t :: pfs -> all_param_filters pfs >>| fun pfs' -> (n, `Text (t, pfs'))
+        | `Timerange t :: pfs ->
+          let* pfs' = all_param_filters pfs in
+          Ok (n, `Range (t, pfs'))
+        | `Text_match t :: pfs ->
+          let* pfs' = all_param_filters pfs in
+          Ok (n, `Text (t, pfs'))
         | _ -> Error "Invalid prop-filter.")
     (name_ns "prop-filter" caldav_ns >>~ extract_attributes)
     (is_not_defined_parser ||| time_range_parser ||| text_match_parser ||| param_filter_parser)
@@ -685,10 +702,10 @@ let rec comp_filter_parser tree : (component_filter, string) result =
          | [] -> Ok (n, `Is_defined)
          | [ `Is_not_defined ] -> Ok (n, `Is_not_defined)
          | `Timerange (a, b)::xs ->
-           f xs >>= fun (props, comps) ->
+           let* props, comps = f xs in
            Ok (n, `Comp_filter (Some (a, b), props, comps))
          | xs ->
-           f xs >>= fun (props, comps) ->
+           let* props, comps = f xs in
            Ok (n, `Comp_filter (None, props, comps)))
     (name_ns "comp-filter" caldav_ns >>~ extract_attributes)
     (is_not_defined_parser ||| time_range_parser
@@ -743,13 +760,13 @@ let proppatch_prop_parser f =
 
 let set_parser : tree -> ([>`Set of attribute list * fqname * tree list] list, string) result =
   tree_lift (* exactly one prop tag, but a list of property trees below that tag *)
-    (fun _ c -> exactly_one c >>| List.map (fun k -> `Set k))
+    (fun _ c -> let* ks = exactly_one c in Ok (List.map (fun k -> `Set k) ks))
     (name_ns "set" dav_ns)
     (proppatch_prop_parser extract_name_value)
 
 let remove_parser : tree -> ([>`Remove of fqname] list, string) result =
   tree_lift (* exactly one prop tag, but a list of property trees below that tag *)
-    (fun _ c -> exactly_one c >>| List.map (fun k -> `Remove k))
+    (fun _ c -> let* ks = exactly_one c in Ok (List.map (fun k -> `Remove k) ks))
     (name_ns "remove" dav_ns)
     (proppatch_prop_parser extract_ns_name)
 

test/test.ml
@@ -1304,7 +1304,10 @@ let write_and_update_parent_mtime () =
       Fs.mkdir fs (`Dir ["principals"]) dir_props >>= fun _ ->
       Fs.mkdir fs (`Dir ["principals" ; "karl"]) dir_props >>= fun _ ->
       Fs.mkdir fs (`Dir ["parent"]) dir_props'' >>= fun _ -> (* getlastmodified needs to be updated since we wrote a child in parent *)
-      let ics = Astring.String.fold_left (fun str -> function '\n' -> str ^ "\r\n" | c -> str ^ String.make 1 c ) "" ics_example in
+      let ics =
+        let re = Re.compile ( Re.Perl.re "\n" ) in
+        Re.replace re ~f:(fun _ -> "\r\n") ics_example
+      in
       Fs.write fs (`File ["parent"; "child"]) ics file_props >>= fun _ ->
       let user = "karl" in
       let data = ics_example in

commit 4a09a0a6c1caa3c090742df0698eabab56fbcdda

Fix href_to_principal to actually use the stuff after "/principal/".
This was broken by 1e4bf3d1085a675e54ee5b38e8d916c3a55d65ce (Nov 2 2021)
"drop rresult and astring dependency"

src/webdav_api.ml
@@ -1134,8 +1134,15 @@ let href_to_principal principals_directory tree =
     let dlen = String.length dir
     and plen = String.length path
     in
-    if plen >= dlen && String.(equal (sub path 0 dlen) dir) then
-      let p = String.sub path 0 dlen in
+    if plen > dlen && String.(equal (sub path 0 dlen) dir) then
+      let length =
+        let base = plen - dlen in
+        if String.get path (dlen - 1) = '/' then
+          base - 1
+        else
+          base
+      in
+      let p = String.sub path dlen length in
       Ok p
     else
       Error "invalid path"