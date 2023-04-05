f5250e652bc2c93878edf9eb1bb85f9a5813c96d
Hannes Mehnert <hannes@mehnert.org>
2023-04-05 14:16:55 +0200

    unikernel: add monitoring (syslog / metrics)

--- mirage/config.ml
+++ mirage/config.ml
@@ -54,12 +54,77 @@ let tofu =
   Key.(create "tofu" Arg.(flag doc))
 
 let hostname =
-  let doc = Key.Arg.info ~doc:"Hostname to use." [ "host" ] ~docv:"STRING" in
+  let doc = Key.Arg.info ~doc:"Hostname to use." [ "host"; "name" ] ~docv:"STRING" in
   Key.(create "hostname" Arg.(required string doc))
 
 let apple_testable =
   let doc = Key.Arg.info ~doc:"Configure the server to use with Apple CCS CalDAVtester." [ "apple-testable" ] in
   Key.(create "apple_testable" Arg.(flag doc))
+
+let enable_monitoring =
+  let doc = Key.Arg.info
+      ~doc:"Enable monitoring (only available for solo5 targets)"
+      [ "enable-monitoring" ]
+  in
+  Key.(create "enable-monitoring" Arg.(flag ~stage:`Configure doc))
+
+let management_stack =
+  if_impl
+    (Key.value enable_monitoring)
+    (generic_stackv4v6 ~group:"management" (netif ~group:"management" "management"))
+    net
+
+let name = Key.v hostname
+
+let monitoring =
+  let monitor =
+    let doc = Key.Arg.info ~doc:"monitor host IP" ["monitor"] in
+    Key.(v (create "monitor" Arg.(opt (some ip_address) None doc)))
+  in
+  let connect _ modname = function
+    | [ _ ; _ ; stack ] ->
+      Fmt.str "Lwt.return (match %a with\
+               | None -> Logs.warn (fun m -> m \"no monitor specified, not outputting statistics\")\
+               | Some ip -> %s.create ip ~hostname:%a %s)"
+        Key.serialize_call monitor modname
+        Key.serialize_call name stack
+    | _ -> assert false
+  in
+  impl
+    ~packages:[ package "mirage-monitoring" ]
+    ~keys:[ name ; monitor ]
+    ~connect "Mirage_monitoring.Make"
+    (time @-> pclock @-> stackv4v6 @-> job)
+
+let syslog =
+  let syslog =
+    let doc = Key.Arg.info ~doc:"syslog host IP" ["syslog"] in
+    Key.(v (create "syslog" Arg.(opt (some ip_address) None doc)))
+  in
+  let connect _ modname = function
+    | [ console ; _ ; stack ] ->
+      Fmt.str "Lwt.return (match %a with\
+               | None -> Logs.warn (fun m -> m \"no syslog specified, dumping on stdout\")\
+               | Some ip -> Logs.set_reporter (%s.create %s %s ip ~hostname:%a ()))"
+        Key.serialize_call syslog modname console stack
+        Key.serialize_call name
+    | _ -> assert false
+  in
+  impl
+    ~packages:[ package ~sublibs:["mirage"] ~min:"0.3.0" "logs-syslog" ]
+    ~keys:[ name ; syslog ]
+    ~connect "Logs_syslog_mirage.Udp"
+    (console @-> pclock @-> stackv4v6 @-> job)
+
+let optional_monitoring time pclock stack =
+  if_impl (Key.value enable_monitoring)
+    (monitoring $ time $ pclock $ stack)
+    noop
+
+let optional_syslog console pclock stack =
+  if_impl (Key.value enable_monitoring)
+    (syslog $ console $ pclock $ stack)
+    noop
 
 let main =
   let direct_dependencies = [
@@ -88,4 +153,8 @@ let git_client =
        (git_http ~authenticator:tls_authenticator tcp git))
 
 let () =
-  register "caldav" [main $ default_random $ default_posix_clock $ git_client $ certs $ http_srv $ zap ]
+  register "caldav" [
+    optional_syslog default_console default_posix_clock management_stack ;
+    optional_monitoring default_time default_posix_clock management_stack ;
+    main $ default_random $ default_posix_clock $ git_client $ certs $ http_srv $ zap
+  ]
