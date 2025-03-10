fe1d5add291d52a5526a522920cca61206201e64
Hannes Mehnert <hannes@mehnert.org>
2025-03-10 11:43:21 +0100

    update cohttp sources from upstream

--- app/cohttp_test.ml
+++ app/cohttp_test.ml
@@ -1,5 +1,3 @@
-open Cohttp
-
 module type S = sig
   type 'a io
   type ic
@@ -7,26 +5,31 @@ module type S = sig
   type body
 
   type response_action =
-    [ `Expert of Cohttp.Response.t
-                 * (ic
-                    -> oc
-                    -> unit io)
-    | `Response of Cohttp.Response.t * body ]
+    [ `Expert of Http.Response.t * (ic -> oc -> unit io)
+    | `Response of Http.Response.t * body ]
 
-  type spec = Request.t -> body -> response_action io
+  type spec = Http.Request.t -> body -> response_action io
   type async_test = unit -> unit io
 
-  val response : (Response.t * body) -> response_action
-  val expert : ?rsp:Cohttp.Response.t -> (ic -> oc -> unit io) -> spec
-  val const : (Response.t * body) io -> spec
+  val response : Http.Response.t * body -> response_action
+  val expert : ?rsp:Http.Response.t -> (ic -> oc -> unit io) -> spec
+  val const : (Http.Response.t * body) io -> spec
   val response_sequence : spec list -> spec
   val temp_server : ?port:int -> spec -> (Uri.t -> 'a io) -> 'a io
-  val test_server_s : ?port:int -> ?name:string -> spec
-    -> (Uri.t -> (string * async_test) list) -> OUnit.test io
+
+  val test_server_s :
+    ?port:int ->
+    ?name:string ->
+    spec ->
+    (Uri.t -> (string * async_test) list) ->
+    OUnit.test io
+
   val run_async_tests : OUnit.test io -> OUnit.test_results io
 end
 
-let port = ref 9193
+let port =
+  Random.self_init ();
+  ref (1024 + Random.int 40000)
 
 let next_port () =
   let current_port = !port in
@@ -37,7 +40,7 @@ let response_sequence fail responses =
   let xs = ref responses in
   fun req body ->
     match !xs with
-    | x::xs' ->
-      xs := xs';
-      x req body
+    | x :: xs' ->
+        xs := xs';
+        x req body
     | [] -> fail "response_sequence: Server exhausted responses"
--- app/cohttp_test.mli
+++ app/cohttp_test.mli
@@ -1,5 +1,3 @@
-open Cohttp
-
 module type S = sig
   type 'a io
   type ic
@@ -7,42 +5,44 @@ module type S = sig
   type body
 
   type response_action =
-    [ `Expert of Cohttp.Response.t
-                 * (ic
-                    -> oc
-                    -> unit io)
-    | `Response of Cohttp.Response.t * body ]
+    [ `Expert of Http.Response.t * (ic -> oc -> unit io)
+    | `Response of Http.Response.t * body ]
 
+  type spec = Http.Request.t -> body -> response_action io
   (** A server that is being tested must be defined by providing a spec *)
-  type spec = Request.t -> body -> response_action io
 
   type async_test = unit -> unit io
 
-  val response : Response.t * body -> response_action
-  val expert : ?rsp:Response.t -> (ic -> oc -> unit io) -> spec
+  val response : Http.Response.t * body -> response_action
+  val expert : ?rsp:Http.Response.t -> (ic -> oc -> unit io) -> spec
 
+  val const : (Http.Response.t * body) io -> spec
   (** A constant handler that always returns its argument *)
-  val const : (Response.t * body) io -> spec
 
-  (** A server that process requests using the provided specs in sequence
-      and crashes on further reqeusts *)
   val response_sequence : spec list -> spec
+  (** A server that process requests using the provided specs in sequence and
+      crashes on further requests *)
 
+  val temp_server : ?port:int -> spec -> (Uri.t -> 'a io) -> 'a io
   (** Create a temporary server according to spec that lives until the callback
       thread is determined. The uri provided in the callback should be the base
       uri for any requests made to the temp server *)
-  val temp_server : ?port:int -> spec -> (Uri.t -> 'a io) -> 'a io
 
-  (** Create a test suite against a server defined by spec. Tests
-      run sequentially. *)
-  val test_server_s : ?port:int -> ?name:string -> spec
-    -> (Uri.t -> (string * async_test) list) -> OUnit.test io
+  val test_server_s :
+    ?port:int ->
+    ?name:string ->
+    spec ->
+    (Uri.t -> (string * async_test) list) ->
+    OUnit.test io
+  (** Create a test suite against a server defined by spec. Tests run
+      sequentially. *)
 
-  (** Run an async unit test and return and print the result *)
   val run_async_tests : OUnit.test io -> OUnit.test_results io
+  (** Run an async unit test and return and print the result *)
 end
 
-(** Internal API. Subject to breakage *)
 val next_port : unit -> int
-val response_sequence : (string -> 'a) -> ('b -> 'c -> 'a) list
-  -> 'b -> 'c -> 'a
+(** Internal API. Subject to breakage *)
+
+val response_sequence :
+  (string -> 'a) -> ('b -> 'c -> 'a) list -> 'b -> 'c -> 'a
